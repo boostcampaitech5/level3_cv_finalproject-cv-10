c783f13260cf0451fb51bb516bcd3aef73dc7e45
bogeoung <naturalbb360@gmail.com>
2023-07-26 04:55:34 +0000

    check img type

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from streamlit_webrtc import webrtc_streamer
 
 
 def show_app(image_placeholder, img):
+    print(type(img))
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     image_placeholder.image(img_rgb)
 
